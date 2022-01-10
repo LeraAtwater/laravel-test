d44093e9aa65ad8dfc09b940a1e91b8e6df979dc
Lera Atwater <lera.atwater@icfnext.com>
2022-01-10 11:30:27 -0700

    conditional rendering

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 @section('title', $post['title'])
 @section('content')
-
+@if($post['is_new'])
+  <div>
+    <p>This is a new post! Using if directive</p>
+  </div>
+@elseif(!$post['is_new'])
+  <div>
+    <p>This post is old.</p>
+  </div>
+@endif
   <h1>{{ $post['title'] }}</h1>
   <p>{{ $post['content'] }}</p>
 
