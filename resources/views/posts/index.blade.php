617626b3e12b3f13853f46fa6012fae3a9a3f179
Lera Atwater <lera.atwater@icfnext.com>
2022-01-10 13:04:40 -0700

    template partials

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,16 +5,7 @@
 @section('content')
 
     @forelse ($posts as $key => $post)
-    {{-- $key is the index number --}}
-    {{-- break is to tell the loop to stop at certain index # --}}
-    {{-- @break($key == 2) --}}
-    {{-- render after certain key --}}
-    {{-- @continue($key == 1) --}}
-      @if ($loop->even)
-        <div><h2>{{ $key }}. {{ $post['title'] }}</h2></div>
-      @else
-      <div style="background-color: silver"><h2>{{ $key }}. {{ $post['title'] }}</h2></div>
-      @endif
+      @include('posts.partials.post')
     @empty
     <div>
       <p>No posts found!</p>
---
+++ resources/views/posts/partials/post.blade.php
@@ -0,0 +1,10 @@
+{{-- $key is the index number --}}
+    {{-- break is to tell the loop to stop at certain index # --}}
+    {{-- @break($key == 2) --}}
+    {{-- render after certain key --}}
+    {{-- @continue($key == 1) --}}
+    @if ($loop->even)
+    <div><h2>{{ $key }}. {{ $post['title'] }}</h2></div>
+  @else
+  <div style="background-color: silver"><h2>{{ $key }}. {{ $post['title'] }}</h2></div>
+  @endif
