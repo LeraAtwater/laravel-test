c3fd1aabf67b5eb2e6331ebab171d5e8317edb3d
Lera Atwater <lera.atwater@icfnext.com>
2022-01-10 11:38:17 -0700

    more conditionals

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,14 +3,28 @@
 @section('content')
 @if($post['is_new'])
   <div>
-    <p>This is a new post! Using if directive</p>
+    <p>This is a new post... using if</p>
   </div>
+{{-- can also use plain @else instead of @elseif, since this is a boolean --}}
 @elseif(!$post['is_new'])
   <div>
-    <p>This post is old.</p>
+    <p>This post is old... using elseif/else</p>
   </div>
 @endif
+
+@unless ($post['is_new'])
+<div></div>
+  <p>This is an old post... using unless.</p>
+</div>
+@endunless
+
   <h1>{{ $post['title'] }}</h1>
   <p>{{ $post['content'] }}</p>
+
+  @isset($post['has_comments'])
+  <div>
+    <p>This post has some comments... using isset</p>
+  </div>
+@endisset
 
 @endsection
